94b5e87cc6d4d6c836187a37a186cbb35ece83be
Ben Shaver <ben.shaver@cynnovative.com>
2022-11-23 12:17:03 -0500

    2021 - day 1

--- aoc21_01/src/main.rs
+++ aoc21_01/src/main.rs
@@ -2,19 +2,42 @@ use aoc_utils::input;
 
 const INPUT: &str = include_str!("../input.txt");
 
-fn part_one() -> String {
-    todo!()
+fn part_one(input: &str) -> usize {
+    let nums: Vec<u32> = input::get_lines_of_type(input);
+    nums.windows(2)
+        .filter(|window| window[1] > window[0])
+        .count()
 }
-fn part_two() -> String {
-    todo!()
+
+fn part_two(input: &str) -> usize {
+    let nums: Vec<u32> = input::get_lines_of_type(input)
+        .windows(3)
+        .map(|window| window.iter().sum())
+        .collect();
+    nums.windows(2)
+        .filter(|window| window[1] > window[0])
+        .count()
 }
 fn main() {
     println!("Head of INPUT:\n{:?}", input::head(INPUT));
-    part_one();
-    part_two();
+    let part_one_solution = part_one(INPUT);
+    println!("Solution to part_one: {}", part_one_solution);
+    let part_two_solution = part_two(INPUT);
+    println!("Solution to part_one: {}", part_two_solution);
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use crate::*;
+
+    #[test]
+    fn part_one_sample() {
+        let sample = "199\n200\n208\n210\n200\n207\n240\n269\n260\n263\n";
+        assert_eq!(part_one(sample), 7);
+    }
+    #[test]
+    fn part_two_sample() {
+        let sample = "199\n200\n208\n210\n200\n207\n240\n269\n260\n263\n";
+        assert_eq!(part_two(sample), 5);
+    }
 }
--- aoc_utils/src/lib.rs
+++ aoc_utils/src/lib.rs
@@ -1,6 +1,23 @@
+// #![warn(missing_docs)]
+
 pub mod input {
+    use std::str::FromStr;
     pub fn head(s: &str) -> Vec<&str> {
         s.lines().take(5).collect()
+    }
+
+    pub fn get_lines_of_type<T>(input: &str) -> Vec<T>
+    where
+        T: FromStr,
+    {
+        let mut res: Vec<T> = Vec::new();
+        for line in input.lines() {
+            match line.parse() {
+                Ok(item) => res.push(item),
+                Err(_) => continue,
+            }
+        }
+        res
     }
 }
 
@@ -33,5 +50,26 @@ mod tests {
             input::head("foo\nbar\nbaz\ndead\nbeef\nfoo"),
             vec!["foo", "bar", "baz", "dead", "beef"]
         )
+    }
+
+    #[test]
+    fn get_lines_of_type_all_ints() {
+        let sample = "1\n2\n3";
+        let result: Vec<u8> = input::get_lines_of_type(sample);
+        assert_eq!(result, vec![1, 2, 3])
+    }
+
+    #[test]
+    fn get_lines_of_type_not_all_ints() {
+        let sample = "1\n2\nthree";
+        let result: Vec<u32> = input::get_lines_of_type(sample);
+        assert_eq!(result, vec![1, 2])
+    }
+
+    #[test]
+    fn get_lines_of_type_not_all_ints_trailing_newline() {
+        let sample = "1\n2\nthree\n";
+        let result: Vec<u32> = input::get_lines_of_type(sample);
+        assert_eq!(result, vec![1, 2])
     }
 }
