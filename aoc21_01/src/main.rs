a3c503e3c0552095534e11def7602c553101148c
Ben Shaver <ben.shaver@cynnovative.com>
2022-11-23 11:07:42 -0500

    utility module input with fn 'head' to view first 5 lines of input string

--- aoc21_01/src/main.rs
+++ aoc21_01/src/main.rs
@@ -1,3 +1,20 @@
+use aoc_utils::input;
+
+const INPUT: &str = include_str!("../input.txt");
+
+fn part_one() -> String {
+    todo!()
+}
+fn part_two() -> String {
+    todo!()
+}
 fn main() {
-    println!("Hello, world!");
+    println!("Head of INPUT:\n{:?}", input::head(INPUT));
+    part_one();
+    part_two();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
 }
--- aoc_utils/src/lib.rs
+++ aoc_utils/src/lib.rs
@@ -1,14 +1,37 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub mod input {
+    pub fn head(s: &str) -> Vec<&str> {
+        s.lines().take(5).collect()
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use crate::input;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn head_empty() {
+        let empty_vec: Vec<&str> = vec![];
+        assert_eq!(input::head(""), empty_vec)
+    }
+
+    #[test]
+    fn head_one() {
+        assert_eq!(input::head("foobar"), vec!["foobar"])
+    }
+
+    #[test]
+    fn head_one_newline() {
+        assert_eq!(input::head("\n"), vec![""])
+    }
+    #[test]
+    fn head_two_newlines() {
+        assert_eq!(input::head("\n\n"), vec!["", ""])
+    }
+    #[test]
+    fn head_six() {
+        assert_eq!(
+            input::head("foo\nbar\nbaz\ndead\nbeef\nfoo"),
+            vec!["foo", "bar", "baz", "dead", "beef"]
+        )
     }
 }
