748374e6850b9f0031c78bde52877f9dd2016b7b
Ben Shaver <ben.shaver@cynnovative.com>
2022-12-09 22:33:03 -0500

    example in documentation

--- aoc_utils/src/lib.rs
+++ aoc_utils/src/lib.rs
@@ -12,4 +12,35 @@ pub mod input;
 pub mod tree;
 
 /// Implementations of 2D mazes.
+///
+/// The `HashMapOccupiedMaze` combined with the `Block` enum can be used to represent a 2D maze
+/// with a single "occupant" of the maze.
+///
+/// # Examples
+/// ```
+/// use aoc_utils::maze::*;
+///
+/// let mut maze = HashMapMaze::new();
+///
+/// maze.add_loc((0,0), Block::Path('.'));
+/// maze.add_loc((0,1), Block::Path('.'));
+/// maze.add_loc((0,2), Block::Wall('x'));
+///
+/// let mut maze = HashMapOccupiedMaze::from_hash_map_maze(maze, (0,0)).unwrap();
+/// maze.add_loc((1,1), Block::Path('$'));
+///
+/// // Maze now looks like this:
+/// //
+/// //         x
+/// //         .$
+/// //         .
+/// //
+/// maze.move_up().unwrap();
+/// maze.move_right().unwrap();
+///
+/// match maze.get_value() {
+///     Block::Path('$') => println!("We found the treasure!"),
+///     _ => panic!()
+/// };
+/// ```
 pub mod maze;
