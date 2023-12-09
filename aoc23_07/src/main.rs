0a08a9222636a411971babefc6672e3826616dec
Ben Shaver <benpshaver@gmail.com>
2023-12-09 14:48:42 -0500

    2023 - day 07 - the truly generic solution blocked by this rust-lang issue: https://github.com/rust-lang/project-const-generics/issues/26

--- aoc23_07/src/main.rs
+++ aoc23_07/src/main.rs
@@ -7,81 +7,130 @@ use std::str::FromStr;
 const INPUT: &str = include_str!("../input.txt");
 
 #[derive(Debug, PartialEq, Eq, Hash)]
-struct Card {
+struct Card<const JOKERS: bool> {
     char: char,
 }
 
-impl PartialOrd for Card {
+impl PartialOrd for Card<false> {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
     }
 }
 
-impl Ord for Card {
-    fn cmp(&self, other: &Self) -> Ordering {
-        let selfc = self.char;
-        let other = other.char;
+impl PartialOrd for Card<true> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
 
-        match (selfc.to_digit(10), other.to_digit(10)) {
-            (None, Some(_)) => Ordering::Greater,
-            (Some(_), None) => Ordering::Less,
-            (Some(s), Some(o)) => s.cmp(&o),
-            (None, None) => {
-                if selfc == other {
-                    Ordering::Equal
-                } else if selfc == 'A' {
-                    Ordering::Greater
-                } else if other == 'A' {
-                    Ordering::Less
-                } else if selfc == 'K' {
-                    Ordering::Greater
-                } else if other == 'K' {
-                    Ordering::Less
-                } else if selfc == 'Q' {
-                    Ordering::Greater
-                } else if other == 'Q' {
-                    Ordering::Less
-                } else if selfc == 'J' {
-                    Ordering::Greater
-                } else if other == 'J' {
-                    Ordering::Less
-                } else {
-                    Ordering::Less
-                }
+// Not yet supported:
+// https://github.com/rust-lang/project-const-generics/issues/26
+// impl<const JOKERS: bool> PartialOrd for Card<JOKERS> {
+//     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+//         Some(self.cmp(other))
+//     }
+// }
+
+fn no_joker_ordering(a: char, b: char) -> Ordering {
+    match (a.to_digit(10), b.to_digit(10)) {
+        (None, Some(_)) => Ordering::Greater,
+        (Some(_), None) => Ordering::Less,
+        (Some(s), Some(o)) => s.cmp(&o),
+        (None, None) => {
+            if a == b {
+                Ordering::Equal
+            } else if a == 'A' {
+                Ordering::Greater
+            } else if b == 'A' {
+                Ordering::Less
+            } else if a == 'K' {
+                Ordering::Greater
+            } else if b == 'K' {
+                Ordering::Less
+            } else if a == 'Q' {
+                Ordering::Greater
+            } else if b == 'Q' {
+                Ordering::Less
+            } else if a == 'J' {
+                Ordering::Greater
+            } else if b == 'J' {
+                Ordering::Less
+            } else {
+                Ordering::Less
             }
         }
     }
 }
 
-trait ToCard {
-    fn to_card(&self) -> Card;
+impl Ord for Card<false> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        let selfc = self.char;
+        let other = other.char;
+
+        no_joker_ordering(selfc, other)
+    }
 }
 
-impl ToCard for char {
-    fn to_card(&self) -> Card {
+impl Ord for Card<true> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        let selfc = self.char;
+        let other = other.char;
+
+        if selfc == 'J' && other != 'J' {
+            return Ordering::Less;
+        }
+        if other == 'J' && selfc != 'J' {
+            return Ordering::Greater;
+        }
+
+        no_joker_ordering(selfc, other)
+    }
+}
+
+trait ToCard<const JOKERS: bool> {
+    fn to_card(&self) -> Card<JOKERS>;
+}
+
+impl ToCard<false> for char {
+    fn to_card(&self) -> Card<false> {
+        Card { char: *self }
+    }
+}
+impl ToCard<true> for char {
+    fn to_card(&self) -> Card<true> {
         Card { char: *self }
     }
 }
 
-#[allow(dead_code)]
 #[derive(Debug, PartialOrd, Ord, PartialEq, Eq)]
 enum HandType<'a> {
-    HighCard(&'a Vec<Card>),
-    OnePair(&'a Vec<Card>),
-    TwoPair(&'a Vec<Card>),
-    ThreeKind(&'a Vec<Card>),
-    FullHouse(&'a Vec<Card>),
-    FourKind(&'a Vec<Card>),
-    FiveKind(&'a Vec<Card>),
+    HighCard(&'a Vec<Card<false>>),
+    OnePair(&'a Vec<Card<false>>),
+    TwoPair(&'a Vec<Card<false>>),
+    ThreeKind(&'a Vec<Card<false>>),
+    FullHouse(&'a Vec<Card<false>>),
+    FourKind(&'a Vec<Card<false>>),
+    FiveKind(&'a Vec<Card<false>>),
+}
+
+#[derive(Debug, PartialOrd, Ord, PartialEq, Eq)]
+enum JokersHandType<'a> {
+    HighCard(&'a Vec<Card<true>>),
+    OnePair(&'a Vec<Card<true>>),
+    TwoPair(&'a Vec<Card<true>>),
+    ThreeKind(&'a Vec<Card<true>>),
+    FullHouse(&'a Vec<Card<true>>),
+    FourKind(&'a Vec<Card<true>>),
+    FiveKind(&'a Vec<Card<true>>),
 }
 
 #[derive(Debug, PartialEq, Eq)]
-struct Hand {
-    cards: Vec<Card>,
+struct Hand<const JOKERS: bool> {
+    cards: Vec<Card<JOKERS>>,
     bid: u32,
 }
 
-impl Hand {
+impl Hand<false> {
     fn get_type(&self) -> HandType {
         let mut map = HashMap::new();
         for card in self.cards.iter() {
@@ -99,6 +148,47 @@ impl Hand {
             [1, 4] => FourKind(&self.cards),
             [5] => FiveKind(&self.cards),
             _ => {
+                panic!("Unexected hand!")
+            }
+        }
+    }
+}
+
+impl Hand<true> {
+    fn get_type(&self) -> JokersHandType {
+        let mut map = HashMap::new();
+        let mut joker_count: u8 = 0;
+        for card in self.cards.iter() {
+            if card.char != 'J' {
+                *map.entry(card).or_insert_with(|| 0) += 1;
+            } else {
+                joker_count += 1;
+            }
+        }
+        let mut counts: Vec<u8> = map.values().map(|n| *n).collect();
+        counts.sort();
+        use JokersHandType::*;
+        match (joker_count, counts.as_slice()) {
+            (0, [1, 1, 1, 1, 1]) => HighCard(&self.cards),
+            (0, [1, 1, 1, 2]) => OnePair(&self.cards),
+            (1, [1, 1, 1, 1]) => OnePair(&self.cards),
+            (0, [1, 2, 2]) => TwoPair(&self.cards),
+            (1, [1, 1, 2]) => ThreeKind(&self.cards),
+            (2, [1, 2]) => FourKind(&self.cards),
+            (2, [1, 1, 1]) => ThreeKind(&self.cards),
+            (0, [1, 1, 3]) => ThreeKind(&self.cards),
+            (3, [1, 1]) => FourKind(&self.cards),
+            (1, [2, 2]) => FullHouse(&self.cards),
+            (0, [2, 3]) => FullHouse(&self.cards),
+            (1, [1, 3]) => FourKind(&self.cards),
+            (0, [1, 4]) => FourKind(&self.cards),
+            (0, [5]) => FiveKind(&self.cards),
+            (1, [4]) => FiveKind(&self.cards),
+            (2, [3]) => FiveKind(&self.cards),
+            (3, [2]) => FiveKind(&self.cards),
+            (4, [1]) => FiveKind(&self.cards),
+            (5, []) => FiveKind(&self.cards),
+            _ => {
                 dbg!(self);
                 dbg!(counts);
                 panic!("Unexected hand!")
@@ -107,13 +197,23 @@ impl Hand {
     }
 }
 
-impl PartialOrd for Hand {
+impl PartialOrd for Hand<false> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+impl PartialOrd for Hand<true> {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
     }
 }
 
-impl Ord for Hand {
+impl Ord for Hand<true> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.get_type().cmp(&other.get_type())
+    }
+}
+impl Ord for Hand<false> {
     fn cmp(&self, other: &Self) -> Ordering {
         self.get_type().cmp(&other.get_type())
     }
@@ -122,7 +222,17 @@ impl Ord for Hand {
 #[derive(Debug, PartialEq, Eq)]
 struct ParseHandError;
 
-impl FromStr for Hand {
+impl FromStr for Hand<false> {
+    type Err = ParseHandError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (cards, bid) = s.split_once(' ').ok_or_else(|| ParseHandError)?;
+        let cards = cards.chars().map(|char| char.to_card()).collect();
+        let bid = bid.parse().or_else(|_| Err(ParseHandError))?;
+        return Ok(Hand { cards, bid });
+    }
+}
+impl FromStr for Hand<true> {
     type Err = ParseHandError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
@@ -133,9 +243,8 @@ impl FromStr for Hand {
     }
 }
 
-#[allow(unused_variables)]
 fn part_one(input: &str) -> u32 {
-    let mut hands: Vec<Hand> = input::get_lines_of_type(input);
+    let mut hands: Vec<Hand<false>> = input::get_lines_of_type(input);
     hands.sort();
     hands
         .iter()
@@ -144,9 +253,14 @@ fn part_one(input: &str) -> u32 {
         .sum()
 }
 
-fn part_two(input: &str) -> usize {
-    #![allow(unused, dead_code)]
-    todo!()
+fn part_two(input: &str) -> u32 {
+    let mut hands: Vec<Hand<true>> = input::get_lines_of_type(input);
+    hands.sort();
+    hands
+        .iter()
+        .enumerate()
+        .map(|(i, h)| (i as u32 + 1) * h.bid)
+        .sum()
 }
 
 fn main() {
@@ -166,13 +280,13 @@ mod tests {
     #[test]
     fn test_card_orderings() {
         let (a, k, q, j, t, nine, two) = (
-            'A'.to_card(),
-            'K'.to_card(),
-            'Q'.to_card(),
-            'J'.to_card(),
-            'T'.to_card(),
-            '9'.to_card(),
-            '2'.to_card(),
+            ToCard::<false>::to_card(&'A'),
+            ToCard::<false>::to_card(&'K'),
+            ToCard::<false>::to_card(&'Q'),
+            ToCard::<false>::to_card(&'J'),
+            ToCard::<false>::to_card(&'T'),
+            ToCard::<false>::to_card(&'9'),
+            ToCard::<false>::to_card(&'2'),
         );
         assert!(a > k);
         assert!(a > t);
@@ -221,17 +335,25 @@ mod tests {
             bid: 300,
         };
 
-        assert_eq!(hand, "AAAAA 300".parse::<Hand>().unwrap());
+        assert_eq!(hand, "AAAAA 300".parse::<Hand<false>>().unwrap());
 
         assert_eq!(hand.get_type(), HandType::FiveKind(&hand.cards))
     }
 
     #[test]
     fn test_hand_ordering() {
-        let a: Hand = "KTJJT 220".parse().unwrap();
-        let b: Hand = "QQQJA 483".parse().unwrap();
+        let a: Hand<false> = "KTJJT 220".parse().unwrap();
+        let b: Hand<false> = "QQQJA 483".parse().unwrap();
 
         assert!(b > a);
+    }
+
+    #[test]
+    fn test_hand_ordering_with_jokers() {
+        let a: Hand<true> = "KTJJT 220".parse().unwrap();
+        let b: Hand<true> = "QQQJA 483".parse().unwrap();
+
+        assert!(b < a);
     }
 
     #[test]
@@ -241,6 +363,6 @@ mod tests {
 
     #[test]
     fn part_two_sample() {
-        assert_eq!(part_one(SAMPLE), 5905)
+        assert_eq!(part_two(SAMPLE), 5905)
     }
 }
